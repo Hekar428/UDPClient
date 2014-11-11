0493cb6304c261d337c2e2171ef49362b8bfd0d9
Chase Hoselle <choselle@yahoo.com>
2014-11-11 01:05:52 -0600

    fixed up some small issues

--- UDPPingerClient.py
+++ UDPPingerClient.py
@@ -16,19 +16,19 @@ total_packets = 0.0
 
 clientSocket.settimeout(1)
 
-ping = 0.0
+ping = 0
 
-while (ping < 10.0):
-	ping = ping + 1.0
+while (ping < 10):
+	ping = ping + 1
 	start = time.clock()
 	message = "Ping " + str(ping) + " " + str(strftime("%H:%M:%S"))
 	clientSocket.sendto(message, (serverName, serverPort))
 	try:
-		modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-		print modifiedMessage.strip(), "\n", serverAddress
+		message, serverAddress = clientSocket.recvfrom(2048)
+		print message
 
 		elapsed = ((time.clock() - start) * 1000)
-		print elapsed, " ms round trip"
+		print elapsed, "ms round trip"
 		if elapsed < min_trip or min_trip == 0.0:
 			min_trip = elapsed
 		if elapsed > max_trip or max_trip == 0.0:
@@ -38,6 +38,7 @@ while (ping < 10.0):
 		packet_lost = packet_lost + 1.0
 		print "Request timed out"
 	total_packets = total_packets + 1.0
+	print ''
 clientSocket.close()
 print "Round Trip Time: max-> " + str(max_trip) + " min-> " + str(min_trip) + " average-> " + str(average_trip/total_packets)
 print "Packet Loss Percentage: " + str(packet_lost/total_packets * 100) + "%"
