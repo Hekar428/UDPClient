4c2a6a2842ddbf95b062fb676458546ba6023d68
Hekar428 <choselle@Macbook.local>
2014-11-11 12:02:05 -0600

    Started adding comments

--- UDPPingerClient.py
+++ UDPPingerClient.py
@@ -3,10 +3,14 @@ from socket import *
 
 from time import strftime
 
+#Set serverName and Port
 serverName = "localhost"
 serverPort = 12000
+
+#Bind socket
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 
+#Some variables for finding min, max, average and packet loss rate
 min_trip = 0.0
 max_trip = 0.0
 average_trip = 0.0
@@ -14,6 +18,7 @@ average_trip = 0.0
 packet_lost = 0.0
 total_packets = 0.0
 
+#Set timeout to one second
 clientSocket.settimeout(1)
 
 ping = 0
